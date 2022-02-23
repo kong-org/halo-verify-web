a0078e51c9fa55121abe356a72d16aad6bc573cf
Robby Klein <hello@robbyk.com>
2022-02-23 13:20:56 -0800

    add

--- src/stores/deviceStore.tsx
+++ src/stores/deviceStore.tsx
@@ -1,14 +1,46 @@
 import create from 'zustand'
 import URL from 'url-parse'
+import axios from 'axios'
 
 type TDeviceStore = {
+  primaryKey: string
   init(): void
+  linkHalo(): void
 }
 
 const deviceStore = create<TDeviceStore>((set) => ({
+  primaryKey: '',
+
   init: () => {
     const url = URL(window.location.href, true)
-    console.log(url)
+
+    if (url?.query?.cmd) {
+      set({ primaryKey: url?.query?.cmd })
+    } else {
+      alert('no primary key!')
+    }
+  },
+
+  linkHalo: () => {
+    // Get
+    axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+      console.log(res.data)
+    })
+
+    // Post
+    const data = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }
+
+    const headers = {
+      Robby: 'is sooooooo cool',
+    }
+
+    axios.post('https://jsonplaceholder.typicode.com/posts', data, { headers }).then((res) => {
+      console.log(res.data)
+    })
   },
 }))
 
